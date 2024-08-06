377e793b2a69947d27cac796469fac9d9772494b
omtorano <olivia.torano@gmail.com>
2024-08-06 09:33:27 -0400

    add test RRBS feature map

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' Subset of fathead minnow EE2 exposure RNAseq data collected by MIB (Janine Fetke et al.).
 #'
 #' @format ## `test_rnaseq_counts`
-#' A data frame of raw RNAseq counts with 10,000 gene features (rows) and 56 samples (columns).
+#' A data frame of raw RNAseq counts with 500 gene features (rows) and 56 samples (columns).
 #'
 "test_rnaseq_counts"
 
@@ -12,7 +12,7 @@
 #' Subset of fathead minnow EE2 exposure RRBS data collected by MIB (Janine Fetke et al.)
 #'
 #' @format ## `test_rrbs_mvals`
-#' A data frame of RRBS M Values with 10,000 feautres (rows) and 56 samples (columns)
+#' A data frame of RRBS M Values with 5,000 feautres (rows) and 56 samples (columns)
 #'
 "test_rrbs_mvals"
 
@@ -28,3 +28,12 @@
 #'  }
 #'
 "test_meta"
+
+#' RRBS feature map for example data
+#'
+#' Allows matching fathead minnow scaffold location to gene name
+#'
+#' @format ## `test_RRBS_feature_map`
+#' A data frame matching fathead minnow scaffold location to gene name
+#'
+"test_RRBS_feature_map"
