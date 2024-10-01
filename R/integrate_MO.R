0f32f9ffb6fe38d3dfe9f383f8c176fb8a95447e
omtorano <olivia.torano@gmail.com>
2024-10-01 16:09:39 -0400

    update readme test section

--- R/integrate_MO.R
+++ R/integrate_MO.R
@@ -244,7 +244,7 @@ integrate_MO <- function(int_method = c("sPLS-DA", "WGCNA", "SNF"), RRBS_feature
         modassignment[[i]] <- WGCNA::moduleEigengenes(X[[i]], mergedColors)$validColors
         utils::write.table(lowcount_omics_MEs[[i]], paste0(cdir, "/", paste0(i, "_output"), "/", "Module_Eigengenes_", i, ".txt"))
         svglite::svglite(file = paste0(cdir, "/", paste0(i, "_output"), "/", paste0("Module_Eigengenes_avgExp_heatplot_", i, ".svg")))
-        stats::heatmap(as.matrix(WGCNA::moduleEigengenes(X[[i]], mergedColors)$averageExpr), main = paste("Normalized module feature values", i),
+        stats::heatmap(as.matrix(WGCNA::moduleEigengenes(X[[i]], mergedColors)$averageExpr), main = paste("Normalized module feature expression values", i),
                        margins = c(8, 8), cexCol = 0.8)
         varExplained <- WGCNA::moduleEigengenes(X[[i]], mergedColors)$varExplained
         grDevices::dev.off()
@@ -322,7 +322,7 @@ integrate_MO <- function(int_method = c("sPLS-DA", "WGCNA", "SNF"), RRBS_feature
         modassignment[[i]] <- WGCNA::moduleEigengenes(X[[i]], bwModuleColors)$validColors
         utils::write.table(block_MEs[[i]], paste0(cdir, "/", paste0(i, "_output"), "/", "Module_Eigengenes_", i, ".txt"))
         svglite::svglite(file = paste0(cdir, "/", paste0(i, "_output"), "/", paste0("Module_Eigengenes_avgExp_heatplot_", i, ".svg")))
-        stats::heatmap(as.matrix(WGCNA::moduleEigengenes(X[[i]], bwModuleColors)$averageExpr), main = paste("Normalized module feature values", i),
+        stats::heatmap(as.matrix(WGCNA::moduleEigengenes(X[[i]], bwModuleColors)$averageExpr), main = paste("Normalized module feature expression values", i),
                        margins = c(8, 8), cexCol = 0.8)
         varExplained <- WGCNA::moduleEigengenes(X[[i]], bwModuleColors)$varExplained
         grDevices::dev.off()
